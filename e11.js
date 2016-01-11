f7f167b87a7bc9719975de7639358f028c624c94
William <lhc.simaoji@gmail.com>
2016-01-12 00:34:24 +0800

    add exercise 11.

---
+++ e11.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  var stream = fs.createReadStream(process.argv[3]);
+  stream.pipe(res);
+});
+
+server.listen(process.argv[2]);
